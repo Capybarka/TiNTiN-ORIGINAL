9d51bcca6e3e3ad234089bf049fb4c6c6098b6c5
Capybarka <marrinna03@mail.ru>
2024-05-16 23:56:55 +0300

    #

--- Скрипты/Reservation.js
+++ Скрипты/Reservation.js
@@ -1,31 +1,48 @@
 "use strict";
-
+// поиск нужных элементов по их селекторам
 const reservationBtn = document.querySelector(".reservation-btn");
 const reservationForm = document.querySelector(".reservation_container")
 const inputContainer = document.querySelector(".input-container");
 const reservationDateElement = document.querySelector(".reservation_message_date");
 const messageContainer = document.querySelector(".reservation_message");
 
+// добавление события нажатия на кнопку "забронировать"
 reservationBtn.addEventListener('click', function (event) {
+    // reservationForm - блок с формой
+    // если у формы есть класс hidden , то он удаляется, если нет, то добавляется
     reservationForm.classList.toggle("hidden");
+
     if (!messageContainer.classList.contains("hidden")) {
+        // messageContainer - блок с сообщением
+        // если у блока есть класс , то он переключается, т.е. удаляется, 
+        // так как при повторном нажатии на кнопку "забронировать" надо, чтобы сообщение о бронировани не было видно
         messageContainer.classList.toggle("hidden");
     }
 })
 
+//   высчитывание даты
+
+// кнопка "готово"
 const btnReady = document.querySelector(".reservation_form-btn");
 
+// добавление события нажатия на кнопку "готово"
 btnReady.addEventListener('click', function (event) {
+    // убираем форму и показываем сообщение
     reservationForm.classList.toggle("hidden");
     messageContainer.classList.toggle("hidden");
 
     let date = new Date();
+    // получаем текущую дату и добавляем два дня
     date.setDate(date.getDate() + 2);
 
+    // получаем день
     let day = date.getDate();
+    // получаем месяц (добавляем 1 т.к. индексация начинается с 0)
     let month = date.getMonth() + 1;
+    // получаем год
     let year = date.getFullYear();
 
+    // для красоты добавляем 0 для месяцев < 10
     if (month < 10) month = "0" + month;
 
     reservationDateElement.textContent = day + "-" + month + "-" + year;
--- Скрипты/interactivity.js
+++ Скрипты/interactivity.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     // эффект при наведении на ссылки в навигации
     $('.nav-link').on('mouseenter', function () {
         $(this).css({
+            // изменение стилей
             "background-color": "rgb(63, 63, 63)",
             "border-radius": "1rem",
             "padding": "0.5rem",
@@ -22,18 +23,23 @@ $(document).ready(function () {
 
     // анимация карточек категорий
     $('.categoty_card').each(function (index) {
-        $(this).delay(index * 200).animate(
-            {
-                opacity: 1,
-            }, 500);
+        // each() выполняет указанную функцию для каждого элемента в выборке
+        $(this).delay(index * 150).animate({ opacity: 1, }, 450);
+        // .delay(index * 150) добавляет задержку перед запуском анимации.
+        // Время задержки рассчитывается для каждой карточки, увеличиваясь с увеличением индекса
+
+        // метод animate запускает анимацию изменения стилей элемента - изменяет прозрачность 
+        // элемента на полную (opacity: 1) за 450 миллисекунд.
     });
 
     // фокус на инпутах в форме
     $('#reservation_name').on('focus', function () {
+        // при фокусе переключаем класс
         $(this).toggleClass('focus')
     })
 
     $('#reservation_name').on('blur', function () {
+        // при уведении фокуса переключаем класс обратно
         $(this).toggleClass('focus')
     })
 
